bad1e0302d253ad446abf0ae4e2ba562a4c1f9f2
ayaahmedlotfy <ayaahmedlotfy@outlook.com>
2024-07-18 14:02:25 +0300

    minimize image size

--- src/Components/GameCard.tsx
+++ src/Components/GameCard.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { Game } from '../hooks/useGames'
 import IconList from './IconList'
 import CriticScore from './CriticScore'
+import getImageMinimized from '../services/image-url'
 
 interface Props{
     game:Game
@@ -10,7 +11,7 @@ interface Props{
 const GameCard = ({game}:Props) => {
   return (
    <Card borderRadius={10} overflow='hidden'>
-    <Image src={game.background_image}/>
+    <Image src={getImageMinimized(game.background_image)}/>
     <CardBody>
         <Heading  fontSize='2xl' >{game.name}</Heading>
         <HStack justifyContent='space-between'>
---
+++ src/services/image-url.ts
@@ -0,0 +1,7 @@
+const getImageMinimized=(url:string)=>{
+    const target="media/";
+   const length= url.indexOf(target)+target.length;
+//    to get from start of url till /media and concatenate with crop/400/600 to minimize image and concatenate the least of url
+return url.slice(0,length)+"crop/600/400/"+url.slice(length);
+}
+export default getImageMinimized;
